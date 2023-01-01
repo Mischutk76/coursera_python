223adb3fa2547fb461eccc2d41fb62541b993a03
Micha <you@example.com>
2023-01-01 11:21:18 +0300

    edit main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-n = int(input())
-x, y = 0, 1
-if n == 0:
-    print(x)
-elif n == 1:
-    print(y)
-else:
-    i = 2
-    while i <= n:
-        x, y = y, x + y
-        i += 1
-    print(y)
+print(1)
